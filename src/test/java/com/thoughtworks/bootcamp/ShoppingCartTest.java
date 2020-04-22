9a82a1fc17fc5f9a1359fe23491c8806d75a0168
Avani Kale <avani.kale@thoughtworks.com>
2020-04-22 23:58:43 +0530

    [Avani] Green 2.5- should return cart total of three apple and three mask .

--- src/test/java/com/thoughtworks/bootcamp/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/bootcamp/ShoppingCartTest.java
@@ -63,5 +63,20 @@ public class ShoppingCartTest {
         Assertions.assertEquals(2.98, cartTotal);
 
     }
+    @Test
+    void givenEmptyShoppingCart_whenAddThreeMaskAndThreeApple_thenShouldReturnTotalCartPriceEightPointNineFour() {
+        Product productOne = new Product("apple", 3, 0.99);
+        Product productTwo = new Product("mask", 3, 1.99);
+
+        ArrayList<Product> products = new ArrayList<Product>();
+        products.add(productOne);
+        products.add(productTwo);
+
+        ShoppingCart shoppingCart = new ShoppingCart(products);
+        double cartTotal = shoppingCart.calculateTotalCart();
+
+        Assertions.assertEquals(8.94, cartTotal);
+
+    }
 
 }
