c242d1ff9a76756b8e22aa3f03183c5719588098
Avani Kale <avani.kale@thoughtworks.com>
2020-04-22 22:50:48 +0530

    [Avani] Refactor 1.5- change method name calculate cart total to calculate total cart and change data member name apple to product name.

--- src/main/java/com/thoughtworks/bootcamp/ShoppingCart.java
+++ src/main/java/com/thoughtworks/bootcamp/ShoppingCart.java
@@ -1,17 +1,18 @@
 package com.thoughtworks.bootcamp;
 
 public class ShoppingCart {
-    private String apple;
+    private String productName;
     private int quantity;
     private double price;
 
-    public ShoppingCart(String apple, int quantity, double price) {
-        this.apple = apple;
+    public ShoppingCart(String productName, int quantity, double price) {
+        this.productName = productName;
         this.quantity = quantity;
         this.price = price;
     }
 
-    public double calculateCartTotal() {
-        return quantity * price;
+    public double calculateTotalCart() {
+        double cartTotal = quantity * price;
+        return cartTotal;
     }
 }
--- src/test/java/com/thoughtworks/bootcamp/ShoppingCartTest.java
+++ src/test/java/com/thoughtworks/bootcamp/ShoppingCartTest.java
@@ -8,7 +8,7 @@ public class ShoppingCartTest {
     void givenEmptyShoppingCart_whenAddOneApple_thenShouldReturnTotalCartPriceZeroPointNineNine() {
         ShoppingCart shoppingCart = new ShoppingCart("apple", 1, 0.99);
 
-        double cartTotal = shoppingCart.calculateCartTotal();
+        double cartTotal = shoppingCart.calculateTotalCart();
 
         Assertions.assertEquals(0.99, cartTotal);
 
@@ -18,7 +18,7 @@ public class ShoppingCartTest {
     void givenEmptyShoppingCart_whenAddFiveApple_thenShouldReturnTotalCartPriceZeroPointNineNine() {
         ShoppingCart shoppingCart = new ShoppingCart("apple", 5, 0.99);
 
-        double cartTotal = shoppingCart.calculateCartTotal();
+        double cartTotal = shoppingCart.calculateTotalCart();
 
         Assertions.assertEquals(4.95, cartTotal);
 
